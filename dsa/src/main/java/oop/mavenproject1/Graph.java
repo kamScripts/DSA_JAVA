69f0a2e9b9a825068c7cc36c3983532a78e3e507
kamScripts <k.gebski1@gmail.com>
2025-12-11 15:51:11 +0000

    feat:(Graph) add_random_weigts() feat:(Graph) adj_list(x,y)

--- dsa/src/main/java/oop/mavenproject1/Graph.java
+++ dsa/src/main/java/oop/mavenproject1/Graph.java
@@ -7,14 +7,16 @@
  */
 package oop.mavenproject1;
 
+import java.util.Random;
+
 /**
  *
  * @author KamScripts
  */
 public class Graph {
     private int width, height;
-    private int [][] weights;
-    
+    private int [][] weights; // weights [[cost_right,cost_down],]
+    static int [] COSTS = {1, 2, 3, 9};
     public Graph (int w, int h) {
         width = w;
         height = h;
@@ -45,14 +47,48 @@ public class Graph {
         this.weights = weights;
     }
     public int index(int x, int y){
-        return y * width +1;    // Returns index of flat array
+        return y * width +x;    // Returns index of flat array
+    }
+    public void add_random_weights(){
+        
+        System.out.println(weights[0][0]);
+        Random rnd = new Random();
+        for (int i=0; i<width*height; i++){
+            
+            weights[i][0] = COSTS[rnd.nextInt(COSTS.length)];
+            weights[i][1] = COSTS[rnd.nextInt(COSTS.length)];
+        }
+    }
+    public int[] adj_list(int x, int y) {
+        // Adjacency list of cell[x][y]
+        // Return: [left,right,up,down]
+        int idx = index(x,y);
+        int[]adj = new int [4];
+        int []weight = weights[idx];
+        if (x > 0 || weights[index(x-1,y)][0] < 9){ //left
+            adj[0] = weights[index(x-1,y)][0];
+        }        
+        if (x+1 < width || weight[0] < 9) { //right
+           adj[1] = weight[0];
+        }
+        if (y > 0 || weights[index(x,y-1)][1] < 9) { //up
+            adj[2] = weights[index(x,y-1)][1];
+        }
+        if(y+1 < height || weight[1] < 9) { //down
+            adj[3] = weight[1];
+        }
+        return adj;
+
+        
     }
     public void printer(){
         
         for (int i=0; i<getHeight(); i++){
             for (int j=0; j<getWidth(); j++) {
                 System.out.print(' ');
+
                 System.out.print('O');
+
                 System.out.print(' ');
             }
             System.out.println();
--- dsa/src/main/java/oop/mavenproject1/dsa.java
+++ dsa/src/main/java/oop/mavenproject1/dsa.java
@@ -7,6 +7,12 @@ public class dsa {
 
     public static void main(String[] args) {
         Graph g = new Graph(10,10);
+        g.add_random_weights();
+        int [] adjOfPoint = g.adj_list(1,0);
         g.printer();
+        
+        for (int i : adjOfPoint) { //left>right>up>down
+            System.out.println(i);
+        }
     }
 }
